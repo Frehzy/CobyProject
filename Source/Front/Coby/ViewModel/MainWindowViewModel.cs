44efb057cc3407bf658e697e141a24ebe0fd8c64
Кирилл Сафонов <frehzzzy@gmail.com>
2022-06-05 13:47:26 +0500

    Раскидал по папкам проекты

---
+++ Source/Front/Coby/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Api.Operations;
+
+namespace Coby.ViewModel;
+
+internal class MainWindowViewModel
+{
+    public MainWindowViewModel()
+    {
+        //var order = ModuleOperation.OrderOperation.CreateOrder(Guid.NewGuid(), Guid.NewGuid());
+        var order = ModuleOperation.OrderOperation.GetOrders();
+        /*var session = ModuleOperation.OrderOperation.CreateSession(order.Id);
+        ModuleOperation.GuestOperation.CreateGuest(order, ref session);
+        ModuleOperation.GuestOperation.CreateGuest(order, ref session);
+        ModuleOperation.OrderOperation.SubmitChanges(ref session);
+
+        var orders = ModuleOperation.OrderOperation.GetOrders();*/
+    }
+}
