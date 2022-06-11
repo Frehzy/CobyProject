d1a3c4a0e23f7ea864c435af99c8ce750011911c
Кирилл Сафонов <frehzzzy@gmail.com>
2022-06-11 22:41:48 +0500

    DiscountCache

---
+++ Source/Server/HostData/Cache/Discounts/DiscountCache.cs
@@ -0,0 +1,36 @@
+﻿using Shared.Data;
+using Shared.Exceptions;
+using System.Collections.Concurrent;
+
+namespace HostData.Cache.Discounts;
+
+internal class DiscountCache : IDiscountCache
+{
+    private readonly ConcurrentDictionary<Guid, IDiscount> _discountsCache = new();
+
+    public IReadOnlyCollection<IDiscount> Discounts => _discountsCache.Values.ToList();
+
+    public void AddOrUpdate(IDiscount discount)
+    {
+        if (_discountsCache.TryGetValue(discount.Id, out var discountOnCache) is false)
+            _discountsCache.TryAdd(discount.Id, discount);
+        else
+            _discountsCache.TryUpdate(discount.Id, discount, discountOnCache);
+    }
+
+    public IDiscount GetTableById(Guid discountId)
+    {
+        var result = _discountsCache.TryGetValue(discountId, out var discountOnCache);
+        return result is true
+            ? discountOnCache
+            : throw new EntityNotFoundException(discountId, nameof(IDiscount));
+    }
+
+    public IDiscount RemoveDiscount(Guid discountId)
+    {
+        if (_discountsCache.TryRemove(discountId, out var returnDiscount) is false)
+            throw new EntityNotFoundException(discountId, nameof(ITable));
+
+        return returnDiscount;
+    }
+}
---
+++ Source/Server/HostData/Cache/Discounts/IDiscountCache.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Data;
+
+namespace HostData.Cache.Discounts;
+
+public interface IDiscountCache
+{
+    IReadOnlyCollection<IDiscount> Discounts { get; }
+
+    IDiscount GetTableById(Guid discountId);
+
+    void AddOrUpdate(IDiscount discount);
+
+    IDiscount RemoveDiscount(Guid discountId);
+}
